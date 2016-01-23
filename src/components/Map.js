a37542dda89fc7539f2fcdac172f40336c5e65ac
Scott King <kingsc850@gmail.com>
2016-01-23 17:21:00 -0500

    GOT THE BEST FUNCTION EVER :+1:

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,6 +5,11 @@ import SelectField from 'material-ui/lib/select-field'
 import MenuItem from 'material-ui/lib/menus/menu-item'
 import Slider from 'material-ui/lib/slider'
 import Toggle from 'material-ui/lib/toggle'
+import RaisedButton from 'material-ui/lib/raised-button'
+
+let places = (
+  <div lat={43.701100} lng={-79.416300}>Waterloo</div>
+)
 
 const Map = React.createClass({
   getInitialState () {
@@ -14,7 +19,7 @@ const Map = React.createClass({
       job: '',
       language: 1,
       popDensity: 0.3,
-
+      newData: []
     }
   },
 
@@ -31,6 +36,15 @@ const Map = React.createClass({
     this.setState({language})
   },
 
+  onSearch () {
+    console.log(this.state)
+    let stuff = [{'cityName': 'waterloo', 'grossSalary': '50000', 'image': '...', 'rent': '500', 'disposableIncome': '12500', 'coords': {'la': '1', 'lo': '2'}}, {'cityName': 'hamilton', 'grossSalary': '5000', 'image': '...', 'rent': '500', 'disposableIncome': '12500', 'coords': {'la': '43', 'lo': '-79'}}]
+    this.state.newData = stuff.map((e, i) => {
+      return <div key={i} lat={parseFloat(e.coords.la)} lng={parseFloat(e.coords.lo)}>{e.cityName}</div>
+    })
+    this.setState({la: parseFloat(stuff[0].coords.la), lo: parseFloat(stuff[0].coords.lo)})
+  },
+
   render () {
     const center = [this.state.la, this.state.lo]
     const zoom = 1
@@ -43,7 +57,7 @@ const Map = React.createClass({
                 Job Title
               </td>
               <td>
-                &nbsp;<TextField hintText='ex. Software Developer' value={this.state.job}/><br />
+                &nbsp;<TextField hintText='ex. Software Developer' value={this.state.job} onChange={this.getJob}/><br />
               </td>
             </tr>
             <tr>
@@ -51,7 +65,7 @@ const Map = React.createClass({
                 Language
               </td>
               <td>
-                &nbsp;<SelectField value={this.state.language} onChange={this.handleChange}>
+                &nbsp;<SelectField value={this.state.language.value} onChange={this.handleChange}>
                         <MenuItem value={1} primaryText='English' />
                         <MenuItem value={2} primaryText='French' />
                       </SelectField>
@@ -62,7 +76,7 @@ const Map = React.createClass({
                 Population Density
               </td>
               <td>
-                &nbsp;<Slider defaultValue={this.state.popDensity} value={this.state.popDensity} step={0.1} onChange={getPopDensity()}/>
+                &nbsp;<Slider defaultValue={this.state.popDensity} value={this.state.popDensity} step={0.1}/>
               </td>
             </tr>
             <tr>
@@ -81,10 +95,13 @@ const Map = React.createClass({
                 <Toggle name='Lifestyle' value='lofty' />
               </td>
             </tr>
-          </table>
-        </center>
+          </table><br />
+          <RaisedButton label='Search' primary={true} onClick={this.onSearch}/>
+        </center><br />
         <div style={{width: '100%', height: 400}}>
-          <GoogleMap lat={this.state.la} lng={this.state.lo} center={center} zoom={zoom}/>
+          <GoogleMap id='googs' lat={this.state.la} lng={this.state.lo} center={this.state.center} zoom={zoom}>
+            {this.state.newData}
+          </GoogleMap>
         </div>
       </div>
     )
